05c2a4ce60ae73ff01b1bca4a8b0b5713abf79e2
Dmiphil <dmitrii.phil@gmail.com@git config --global user.email dmitrii.phil@gmail.comgit config --global user.email dmitrii.phil@gmail.com@>
2026-07-27 23:30:20 +0300

    added lemmatization

--- import nltk.py
+++ import nltk.py
@@ -1,13 +1,13 @@
+from nltk import translate
 import nltk
 nltk.download('popular')
 nltk.download('punkt')
 nltk.download('stopwords')
-
 nltk.download('punkt_tab')
+nltk.download('omw-1.4')
 
 text = "The quick brown fox jumps over the lazy dog. This is a test sentence."
 tokenizer = nltk.word_tokenize(text)
-
 print(tokenizer)
 
 from nltk.stem import PorterStemmer
@@ -18,3 +18,13 @@ text = "The stemmed form of leaves is leaf"
 tokens = word_tokenize(text)
 stemmed_words = [stemmer.stem(word) for word in tokens]
 print(stemmed_words)
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+lemmatizer = WordNetLemmatizer()
+text = "The lemmatized form of leaves is leaf"
+tokens = word_tokenize(text)
+lemmatized_words = [lemmatizer.lemmatize(word) for word in tokens]
+print(lemmatized_words)
+
