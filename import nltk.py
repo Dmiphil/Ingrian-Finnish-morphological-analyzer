4e34d5ba67983e229cafb6328fe8c98b50788d62
Dmiphil <dmitrii.phil@gmail.com@git config --global user.email dmitrii.phil@gmail.comgit config --global user.email dmitrii.phil@gmail.com@>
2026-07-27 23:26:55 +0300

    added stemmer

--- import nltk.py
+++ import nltk.py
@@ -9,3 +9,12 @@ text = "The quick brown fox jumps over the lazy dog. This is a test sentence."
 tokenizer = nltk.word_tokenize(text)
 
 print(tokenizer)
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+stemmer = PorterStemmer()
+text = "The stemmed form of leaves is leaf"
+tokens = word_tokenize(text)
+stemmed_words = [stemmer.stem(word) for word in tokens]
+print(stemmed_words)
